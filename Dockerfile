ffa65db8ba94ca4fe275a73965dc835e85f012e5
Harry <harryye.private@gmail.com>
2023-03-29 21:28:58 +0800

    update

--- Dockerfile
+++ Dockerfile
@@ -2,17 +2,13 @@ FROM python
 
 COPY chatbot.py /
 COPY requirements.txt /
+COPY DigiCertGlobalRootG2.crt.pem /
 
 RUN pip install pip update
 RUN pip install -r requirements.txt
 
 # telegram
 ENV ACCESS_TOKEN 6272770771:AAErGQoJw6Yuw2HVGMH6Tp_KsTZiLHE8BzY
-
-# redis-lab
-# ENV HOST redis-10924.c299.asia-northeast1-1.gce.cloud.redislabs.com
-# ENV PASSWORD VXmUlR47Ui2uCSRhgtcXhYBjHGL7HRB7
-# ENV REDISPORT 10924
 
 # Azure redis
 ENV HOST chatbotchatgpt.redis.cache.windows.net
--- chatbot.py
+++ chatbot.py
@@ -1,11 +1,14 @@
 from telegram import Update
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
-# import configparser
+import configparser
 import logging
 import redis
 import os
+import openai
+import json
 
 global redis1
+openai.api_key = "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"
 
 def main():
 # Load your token and create an Updater for your Bot
@@ -14,8 +17,8 @@ def main():
     # updater = Updater(token=(config['TELEGRAM']['ACCESS_TOKEN']), use_context=True)
     # dispatcher = updater.dispatcher
 
-    # updater = Updater(token=(os.environ['ACCESS_TOKEN']), use_context=True)
-    updater = Updater(token="6272770771:AAErGQoJw6Yuw2HVGMH6Tp_KsTZiLHE8BzY", use_context=True)
+    updater = Updater(token=(os.environ['ACCESS_TOKEN']), use_context=True)
+    # updater = Updater(token="6272770771:AAErGQoJw6Yuw2HVGMH6Tp_KsTZiLHE8BzY", use_context=True)
     dispatcher = updater.dispatcher
 
     global redis1
@@ -23,57 +26,84 @@ def main():
     #                     password=(config['REDIS']['PASSWORD']), 
     #                     port=(config['REDIS']['REDISPORT']))
 
-    # redis1 = redis.Redis(host=(os.environ['HOST']), password=
-    #                         (os.environ['PASSWORD']), port=(os.environ['REDISPORT']), ssl=True)
-
-    redis1 = redis.Redis(host="chatbotchatgpt.redis.cache.windows.net", 
-                        password="fMxVXGRWg7Z45RXbrKAbypy2ECgJSbAQ0AzCaCzyPNE=", 
-                        port=6380, ssl=True)
+    redis1 = redis.Redis(host=(os.environ['HOST']), password=
+                            (os.environ['PASSWORD']), port=(os.environ['REDISPORT']), ssl=True)
+    # redis1 = redis.Redis(host="chatbotchatgpt.redis.cache.windows.net", 
+    #                     password="fMxVXGRWg7Z45RXbrKAbypy2ECgJSbAQ0AzCaCzyPNE=", 
+    #                     port=6380, ssl=True)
                         
     # You can set this logging module, so you will know when and why things do not work as expected
     logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',level=logging.INFO)
 
-    # register a dispatcher to handle message: here we register an echo dispatcher
-    echo_handler = MessageHandler(Filters.text & (~Filters.command), echo)
-    dispatcher.add_handler(echo_handler)
-
-    # on different commands - answer in Telegram
-    dispatcher.add_handler(CommandHandler("add", add))
-    dispatcher.add_handler(CommandHandler("help", help_command))
-    dispatcher.add_handler(CommandHandler("hello", hello))
+    # register dispatchers to handle message
+    dispatcher.add_handler(MessageHandler(Filters.text & (~Filters.command), chat))     # normal chat
+    dispatcher.add_handler(CommandHandler("newchat", newchat))    # to create a new chat
+    dispatcher.add_handler(CommandHandler("history", history))    # show all the chats before
+    dispatcher.add_handler(CommandHandler("help", help))        # show helps
 
     # To start the bot:
     updater.start_polling()
     updater.idle()
 
+# normal chat
+def chat(update, context):
+    # log
+    # logging.info("Update: " + str(update))
 
-def echo(update, context):
-    reply_message = update.message.text.upper()
-    logging.info("Update: " + str(update))
-    logging.info("context: " + str(context))
+    content = update['message']['text'] # user input
+    ms = [] # all the message, may includes chats before
+
+    key = "user:" + str(update['message']['chat']['id'])    # redis key, in this format 'user:${telegram_id}'
+
+    # if user is exist, get history, else initialize a new chat
+    if redis1.exists(key):
+        ms.extend(json.loads(redis1.get(key)))  # extend is for merge 2 lists, append is unavaiable for merge
+    ms.append({"role": "user", "content": content})
+
+    # invoke ChatGPT
+    rsp = openai.ChatCompletion.create(
+        model = "gpt-3.5-turbo",
+        messages = ms
+    )
+
+    # include the latest response and set it into redis
+    ms.append({"role": "assistant", "content": rsp["choices"][0]["message"]["content"]})
+    redis1.set(key, json.dumps(ms))
+
+    # reply to user
+    reply_message = rsp["choices"][0]["message"]["content"] 
     context.bot.send_message(chat_id=update.effective_chat.id, text= reply_message)
 
+def help(update: Update, context: CallbackContext) -> None:
+    reply = """
+/help : get help from bot\n
+/newchat : begin a new bot\n
+/history : get history of this chat
+    """
+    update.message.reply_text(reply)
 
-# Define a few command handlers. These usually take the two arguments update and
-# context. Error handlers also receive the raised TelegramError object in error.
-def help_command(update: Update, context: CallbackContext) -> None:
-    """Send a message when the command /help is issued."""
-    update.message.reply_text('Helping you helping you.')
+# begin a new chat
+def newchat(update: Update, context: CallbackContext) -> None:
+    key = "user:" + str(update['message']['chat']['id'])
+    redis1.delete(key)
+    update.message.reply_text("Begin your new chat right now!")
 
+# get history from redis and combine them in readable way
+def history(update: Update, context: CallbackContext) -> None:
+    key = "user:" + str(update['message']['chat']['id'])
+    if not redis1.exists(key):
+        update.message.reply_text("No history, begin your chat right now!")
+        return
+    ms = json.loads(redis1.get(key))
+    reply = ""
+    for m in ms:
+        if m['role'] == 'user':
+            reply += "You:\n" + m['content'] + '\n\n'
+        else:
+            reply += "Bot:\n" + m['content'] + '\n\n\n'
 
-def add(update: Update, context: CallbackContext) -> None:
-    """Send a message when the command /add is issued."""
-    try:
-        global redis1
-        logging.info(context.args[0])
-        msg = context.args[0] # /add keyword <-- this should store the keyword
-        redis1.incr(msg)
-        update.message.reply_text('You have said ' + msg + ' for ' + redis1.get(msg).decode('UTF-8') + ' times.')
-    except (IndexError, ValueError):
-        update.message.reply_text('Usage: /add <keyword>')
+    update.message.reply_text(reply)
 
-def hello(update: Update, context: CallbackContext) -> None:
-    update.message.reply_text('Good day, ' + context.args[0] + "!")
 
 if __name__ == '__main__':
     main()
