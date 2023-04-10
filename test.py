798419a890669c0636cea9f6b5acbfbce49cdb67
Harry <harryye.private@gmail.com>
2023-04-10 23:55:22 +0800

    update

--- test.py
+++
@@ -1,8 +0,0 @@
-import redis
-
-redis1 = redis.Redis(host="chatbotchatgpt.redis.cache.windows.net", 
-                     password="fMxVXGRWg7Z45RXbrKAbypy2ECgJSbAQ0AzCaCzyPNE=", 
-                     port=6380, ssl=True)
-
-redis1.incr(1)
-print(redis1.get(1))
